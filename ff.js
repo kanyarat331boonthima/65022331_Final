8ad190c6849302eebf4ace2637fb8b30cdb395f0
Pwrmsii <naklada5520@gmail.com>
2023-02-22 15:09:15 +0700

    Final

---
+++ ff.js
@@ -0,0 +1,26 @@
+function(){
+    let = document.getElementById('').value;
+    let  = document.getElementById('').value;
+    let  = document.getElementById('').value;    
+    let  = document.getElementById('').value;
+    let  = document.getElementById('')
+    let  = document.createElement('tr')
+    let  = document.createElement('td')
+    let  = document.createElement('td')
+    let  = document.createElement('td')
+    let  = document.createElement('td')
+    if (){
+        alert("ข้อมูลไม่ครบ")
+    }
+    else {
+    .innerText = ;
+    .innerText = ;
+    .innerText = ;
+    .innerText = ;
+    item.appendChild()
+    item.appendChild()
+    item.appendChild()
+    item.appendChild()
+    table.appendChild()
+    }
+}
